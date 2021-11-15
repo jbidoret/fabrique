4050ca62d0e5dcecaea739a24cfcef9f1edf1ce1
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-11-15 09:16:31 +0100

    Fabrique is not fabric

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Fabric</title>
+    <title>Fabrique</title>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
